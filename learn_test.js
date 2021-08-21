5182df19909ae54babffb4c3018e402692e452de
Viplav Nayyar <52647445+Viplav1996@users.noreply.github.com>
2021-08-21 15:47:16 -0400

    Higher order functions

--- learn_test.js
+++ learn_test.js
@@ -1,3 +1,4 @@
+/*
 console.log("Hello World");
 console.log("Program......Start");
 
@@ -16,3 +17,44 @@ function chessBoard(n){
 chessBoard(8);
 
 console.log("End.........Bye!!!");
+*/
+
+
+// ######################    HIGHER ORDER FUCNTION      #########################
+/*
+function noisy(f){
+    return (...args) => {
+        console.log("Calling with args "+ args);
+        let res = f(...args);
+        console.log("Calling with args "+ args + " return "+ res);
+    }
+}
+noisy(Math.min)(3,2,1);              // function funcName(outsideFunc)(InsideFunc);
+
+*/
+/* */
+/* 
+function greaterThan(n){
+    return m => m>n;
+}
+
+let greaterThan10 = greaterThan(10); // fucntion declared as a variable;
+
+console.log(greaterThan10(10));
+*/
+
+function isOdd(n){
+    if(n%2 == 1) return console.log("Number "+ n + " is ODD ");
+    
+}
+
+function repeat(n, func){
+    
+    for(let i=0; i<=n ; i++) {
+        console.log('\n');
+        func(i);
+        console.log('\n');
+    }
+}
+
+repeat(3, isOdd);
